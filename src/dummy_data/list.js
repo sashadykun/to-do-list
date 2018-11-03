e0b3e14591d41ecb9d07b438cf5700cad7f8ec6a
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-02 22:41:36 -0700

    Initial project setup complete with dummy data

---
+++ src/dummy_data/list.js
@@ -0,0 +1,24 @@
+export default [
+    {
+        title: 'Go to store',
+        details: ' Get eggs, bread, milk, and Diet Coke',
+        _id: '001'
+    },
+    {
+        title: 'Lern React',
+        details: 'Some more thinks',
+        _id: '002'
+    },
+    {
+        title: 'Do some do',
+        details: ' Pick some staf',
+        _id: '003'
+    },
+    {
+        title: 'Lern JS',
+        details: ' Functions, classes, forLups',
+        _id: '004'
+    }
+
+
+];
